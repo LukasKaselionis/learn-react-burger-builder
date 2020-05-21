ebf04e3dcbda6a9ae61ee9c81d5c86f02b85b6b6
LukasKaselionis <l.kaselionis@gmail.com>
2020-05-21 11:07:00 +0300

    Create input and dropdown component

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -82,13 +82,27 @@ class ContactData extends Component {
     }
 
     render() {
+        const formElementArray = [];
+
+        for(let key in this.state.orderForm) {
+            formElementArray.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
+
+        console.log(formElementArray)
+
         let form = (
         <form className={classes.Form}>
-            <Input inputtype={'...'} type={'...'} placeholder={'...'} />
-            {/* <Input inputtype="input" type="text" name="name" placeholder="Your name" />
-            <Input inputtype="input" type="email" name="email" placeholder="Your email" />
-            <Input inputtype="input" type="text" name="street" placeholder="Street" />
-            <Input inputtype="input" type="text" name="postal" placeholder="Postal Code" /> */}
+            {formElementArray.map(formElement => (
+                <Input 
+                    key={formElement.id}
+                    elementType={formElement.config.elementType} 
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                />
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
         </form>
         );
