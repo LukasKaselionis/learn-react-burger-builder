6aff2c1594f3b9c4a8a9d2f4702cbe108204b051
LukasKaselionis <l.kaselionis@gmail.com>
2020-06-01 16:14:08 +0300

    Combining Reducers

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -51,7 +51,7 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ings: state.ingredients
+        ings: state.burgerBuilder.ingredients
     }
 }
 
--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -28,7 +28,7 @@ export const puchaseBurger = (orderData) => {
         dispatch(puchaseBurgerStart());
         axios.post('/orders.json', orderData)
         .then(response => {
-            dispatch(purchaseBurgerSuccess(response.data, orderData));
+            dispatch(purchaseBurgerSuccess(response.data.name, orderData));
         })
         .catch(error => {
             dispatch(purchaseBurgerFail(error));
